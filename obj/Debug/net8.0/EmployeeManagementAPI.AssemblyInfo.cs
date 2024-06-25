fb8db5a6ec3f30d4e57428d0b8ffff92e7befc43
unknown <assiatourabi55@gmail.com>
2024-06-25 15:06:56 +0100

    creating model, controller,data and adding dependencies

--- obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0200262607562159bebb92c0e8976d6b03125c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8514571bfe4a50096a04ee79d5b4007bac28841b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
