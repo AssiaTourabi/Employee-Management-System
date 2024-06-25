2e900d31b38f323357ec7988731ee3de918381c2
unknown <assiatourabi55@gmail.com>
2024-06-25 13:00:46 +0100

    creating controller

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeeManagementAPI.Models
 {
     public class Employee
-    { 
+    {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
--- obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1661285de050749149fc3316b2be4636f98db375")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9713ee0ef80ea979cc3cef705a27787166b450a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
